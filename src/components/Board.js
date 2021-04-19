e905ca94d1d108da40ed9cdd6707b917fb67e879
Max Ivanov <waxivanov@gmail.com>
2021-04-19 13:31:27 +0300

    init

---
+++ src/components/Board.js
@@ -0,0 +1,9 @@
+const Board = () => {
+  return (
+    <div>
+      <h2 className='Board-title'>Board</h2>
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+const Square = () => {
+  return (
+    <div>
+      <h2 className='Square-title'>Square</h2>
+    </div>
+  );
+};
+
+export default Square;
