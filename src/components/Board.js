6f7f7bab47dc0e25b0be0a9688193ecc8a316d6a
Max Ivanov <waxivanov@gmail.com>
2021-04-27 16:01:35 +0300

    winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,52 @@
 import { useState } from 'react';
 import Square from './Square';
 
+// TODO
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXiSNext] = useState(true);
 
-  const status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
+  const winner = calculateWinner(squares);
+
+  let status;
+
+  if (winner) {
+    status = `Winner is ${winner}`;
+  } else {
+    status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
   const handleClick = (i) => {
     const copySquares = squares.slice();
+
+    // TODO
+    if (calculateWinner(copySquares) || squares[i]) {
+      return;
+    }
+
     copySquares[i] = xIsNext ? 'X' : 'O';
     setSquares(copySquares);
     setXiSNext((prev) => !prev);
