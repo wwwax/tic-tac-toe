8830f9a9f9e426d2236917ca336a13668f8b2be7
Max Ivanov <waxivanov@gmail.com>
2021-05-12 11:51:32 +0300

    basic game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
 import { useState } from 'react';
 
-export default function App() {
-  const [state, setState] = useState(Array(9).fill(null));
-  console.log(state);
+function winnerChecking(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  const test = () => {
-    setState(null);
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
+export default function App() {
+  const [state, setState] = useState({
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  });
+
+  /**
+   *
+   */
+
+  const winner = winnerChecking(state.squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next: ${state.xIsNext ? 'X' : 'O'}`;
+  }
+
+  /**
+   *
+   */
+
+  const handleClick = (i) => {
+    if (state.squares[i] || winner) {
+      return;
+    }
+
+    const copySquares = state.squares.slice();
+    copySquares[i] = state.xIsNext ? 'X' : 'O';
+    setState((prev) => ({
+      ...prev,
+      squares: copySquares,
+      xIsNext: !prev.xIsNext,
+    }));
   };
 
-  return <h1 onClick={test}>hello</h1>;
+  /**
+   *
+   */
+
+  return (
+    <>
+      <div className='board'>
+        {state.squares.map((square, idx) => (
+          <div className='square' key={idx} onClick={() => handleClick(idx)}>
+            {square}
+          </div>
+        ))}
+      </div>
+
+      <div className='status'>{status}</div>
+    </>
+  );
 }
